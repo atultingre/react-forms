e5120542f407f450336f93d2d33bfb2d4d794c50
ATUL TINGRE <atultingre.work@gmail.com>
2023-05-28 21:38:09 +0530

    login7 css fixed

--- src/Components/Login/Login7/Login7.js
+++ src/Components/Login/Login7/Login7.js
@@ -1,31 +1,29 @@
-import React from 'react'
-import "./Login7.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Login7.css";
+import { Link } from "react-router-dom";
 const Login7 = () => {
   return (
-    <div className='login7-body'>
-         <div className="login-form">
-         <div className="text">
-            LOGIN
-         </div>
-         <form>
-            <div className="field">
-               <div className="fas fa-envelope"></div>
-               <input type="text" placeholder="Email or Phone"/>
-            </div>
-            <div className="field">
-               <div className="fas fa-lock"></div>
-               <input type="password" placeholder="Password"/>
-            </div>
-            <button>LOGIN</button>
-            <div className="link">
-               Not a member?
-               <Link to="#">Signup now</Link>
-            </div>
-         </form>
+    <div className="login7-body">
+      <div className="login7-login-form">
+        <div className="login7-text">LOGIN</div>
+        <form className="login7-form">
+          <div className="login7-field">
+            <div className="fas fa-envelope"></div>
+            <input className="login7-input" type="text" placeholder="Email or Phone" />
+          </div>
+          <div className="login7-field">
+            <div className="fas fa-lock"></div>
+            <input className="login7-input" type="password" placeholder="Password" />
+          </div>
+          <button className="login7-button">LOGIN</button>
+          <div className="login7-link">
+            Not a member?
+            <Link to="#">Signup now</Link>
+          </div>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login7
+export default Login7;
