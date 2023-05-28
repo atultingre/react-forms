34459bd878e32e1f245c5e3c29fcffacaf07e164
ATUL TINGRE <atultingre.work@gmail.com>
2023-05-29 00:01:03 +0530

    Register1 css fixed

--- src/Components/Registration/Registration1/Registration1.js
+++ src/Components/Registration/Registration1/Registration1.js
@@ -3,31 +3,31 @@ import "./Registration1.css";
 import { Link } from "react-router-dom";
 const Registration1 = () => {
   return (
-    <div className="registration1">
-      <div className="wrapper">
-        <h2>Registration</h2>
-        <form action="#">
-          <div className="input-box">
-            <input type="text" placeholder="Enter your name" required />
+    <div className="register1-body">
+      <div className="register1-wrapper">
+        <h2 className="register1-h2">Registration</h2>
+        <form action="#" className="register1-form">
+          <div className="register1-input-box">
+            <input className="register1-input" type="text" placeholder="Enter your name" required />
           </div>
-          <div className="input-box">
-            <input type="text" placeholder="Enter your email" required />
+          <div className="register1-input-box">
+            <input className="register1-input" type="text" placeholder="Enter your email" required />
           </div>
-          <div className="input-box">
-            <input type="password" placeholder="Create password" required />
+          <div className="register1-input-box">
+            <input className="register1-input" type="password" placeholder="Create password" required />
           </div>
-          <div className="input-box">
-            <input type="password" placeholder="Confirm password" required />
+          <div className="register1-input-box">
+            <input className="register1-input" type="password" placeholder="Confirm password" required />
           </div>
-          <div className="policy">
-            <input type="checkbox" />
-            <h3>I accept all terms & condition</h3>
+          <div className="register1-policy">
+            <input className="register1-input" type="checkbox" />
+            <h3 className="register1-h3">I accept all terms & condition</h3>
           </div>
-          <div className="input-box button">
-            <input type="Submit" value="Register Now" />
+          <div className="register1-input-box button">
+            <input className="register1-input" type="Submit" value="Register Now" />
           </div>
-          <div className="text">
-            <h3>
+          <div className="register1-text">
+            <h3 className="register1-h3">
               Already have an account? <Link to="/forms/login1">Login now</Link>
             </h3>
           </div>
