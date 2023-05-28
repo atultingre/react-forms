49c4fb926815f7ed0747575b2940060ed6769595
ATUL TINGRE <atultingre.work@gmail.com>
2023-05-29 02:43:48 +0530

    Registration5 css fixed

--- src/Components/Registration/Registration5/Registration5.js
+++ src/Components/Registration/Registration5/Registration5.js
@@ -26,59 +26,65 @@ const Registration5 = () => {
   links.forEach((link) => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
-      document.querySelector(".forms").classList.toggle("show-signup");
+      document
+        .querySelector(".registration5-forms")
+        .classList.toggle("show-signup");
     });
   });
 
   return (
-    <div className="registration5-form">
-      <section className="registration5-container forms">
-        <div className="form signup">
+    <div className="registration5-body">
+      <section className="registration5-container registration5-forms">
+        <div className="registration5-form1 registration5-signup">
           <div className="form-content">
-            <header>Signup</header>
-            <form action="#">
-              <div className="field input-field">
-                <input type="email" placeholder="Email" className="input" />
+            <header className="registration5-header">Signup</header>
+            <form action="#" className="registration5-form">
+              <div className="registration5-field registration5-input-field">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="input registration5-input"
+                />
               </div>
-              <div className="field input-field">
+              <div className="registration5-field registration5-input-field">
                 <input
                   type="password"
                   placeholder="Create password"
-                  className="password"
+                  className="password registration5-input"
                 />
               </div>
-              <div className="field input-field">
+              <div className="registration5-field registration5-input-field">
                 <input
                   type="password"
                   placeholder="Confirm password"
-                  className="password"
+                  className="password registration5-input"
                 />
                 <i className="bx bx-hide eye-icon"></i>
               </div>
-              <div className="field button-field">
-                <button>Signup</button>
+              <div className="registration5-field button-field">
+                <button className="registration5-button">Signup</button>
               </div>
             </form>
-            <div className="form-link">
-              <span>
-                Already have an account?
+            <div className="registration5-form-link">
+              <span className="registration5-span-link">
+                Already have an account? 
                 <Link to="/login4" className="link login-link">
-                  Login
+                   Login
                 </Link>
               </span>
             </div>
           </div>
-          <div className="line"></div>
-          <div className="media-options">
-            <Link to="#" className="field facebook">
+          <div className="registration5-line"></div>
+          <div className="registration5-media-options">
+            <Link to="#" className="registration5-field facebook">
               <i className="bx bxl-facebook facebook-icon"></i>
-              <span>Login with Facebook</span>
+              <span className="registration5-span"> Login with Facebook</span>
             </Link>
           </div>
-          <div className="media-options">
-            <Link to="#" className="field google">
-              <img src="#" alt="" className="google-img" />
-              <span>Login with Google</span>
+          <div className="registration5-media-options">
+            <Link to="#" className="registration5-field facebook">
+              <i className="bx bxl-google facebook-icon"></i>
+              <span className="registration5-span"> Login with Google</span>
             </Link>
           </div>
         </div>
