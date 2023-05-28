cee7197ad194cf8c608079dbb7247fe662fcfc7d
ATUL TINGRE <atultingre.work@gmail.com>
2023-05-29 02:57:04 +0530

    Registration6 css fixed

--- src/Components/Registration/Registration6/Registration6.js
+++ src/Components/Registration/Registration6/Registration6.js
@@ -5,49 +5,49 @@ const Registration6 = () => {
   return (
     <div className="registration6-body">
        <section className="registration6-container">
-      <header>Registration Form</header>
+      <header className="registration6-header">Registration Form</header>
       <form action="#" className="registration6-form">
-        <div className="input-box">
+        <div className="registration6-input-box">
           <label className="registration6-label">Full Name</label>
           <input className="registration6-input" type="text" placeholder="Enter full name" required />
         </div>
-        <div className="input-box">
+        <div className="registration6-input-box">
           <label className="registration6-label">Email Address</label>
           <input className="registration6-input" type="text" placeholder="Enter email address" required />
         </div>
-        <div className="column">
-          <div className="input-box">
+        <div className="registration6-column">
+          <div className="registration6-input-box">
             <label className="registration6-label">Phone Number</label>
             <input className="registration6-input" type="number" placeholder="Enter phone number" required />
           </div>
-          <div className="input-box">
+          <div className="registration6-input-box">
             <label className="registration6-label">Birth Date</label>
             <input className="registration6-input" type="date" placeholder="Enter birth date" required />
           </div>
         </div>
-        <div className="gender-box">
-          <h3>Gender</h3>
+        <div className="registration6-gender-box">
+          <h3 className="registration6-h3">Gender</h3>
           <div className="gender-option">
-            <div className="gender">
+            <div className="registration6-gender">
               <input className="registration6-input" type="radio" id="check-male" name="gender" checked />
               <label className="registration6-label" for="check-male">male</label>
             </div>
-            <div className="gender">
+            <div className="registration6-gender">
               <input className="registration6-input" type="radio" id="check-female" name="gender" />
               <label className="registration6-label" for="check-female">Female</label>
             </div>
-            <div className="gender">
+            <div className="registration6-gender">
               <input className="registration6-input" type="radio" id="check-other" name="gender" />
               <label className="registration6-label" for="check-other">prefer not to say</label>
             </div>
           </div>
         </div>
-        <div className="input-box address">
+        <div className="registration6-input-box registration6-address">
           <label className="registration6-label">Address</label>
           <input className="registration6-input" type="text" placeholder="Enter street address" required />
           <input className="registration6-input" type="text" placeholder="Enter street address line 2" required />
-          <div className="column">
-            <div className="select-box">
+          <div className="registration6-column">
+            <div className="registration6-select-box">
               <select>
                 <option hidden>Country</option>
                 <option>America</option>
@@ -58,7 +58,7 @@ const Registration6 = () => {
             </div>
             <input className="registration6-input" type="text" placeholder="Enter your city" required />
           </div>
-          <div className="column">
+          <div className="registration6-column">
             <input className="registration6-input" type="text" placeholder="Enter your region" required />
             <input className="registration6-input" type="number" placeholder="Enter postal code" required />
           </div>
