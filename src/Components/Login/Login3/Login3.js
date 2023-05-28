fef5291b2cfeaeca14db93bfa272bd9916917603
ATUL TINGRE <atultingre.work@gmail.com>
2023-05-28 18:09:06 +0530

    Login3 form fixed media queries

--- src/Components/Login/Login3/Login3.js
+++ src/Components/Login/Login3/Login3.js
@@ -1,34 +1,53 @@
-import React from 'react'
-import "./Login3.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Login3.css";
+import { Link } from "react-router-dom";
 const Login3 = () => {
   return (
-    <div className='login3-form'>
-         <div className="login3-container">
-      <form action="#">
-        <div className="title">Login</div>
-        <div className="input-box underline">
-          <input type="text" placeholder="Enter Your Email" required/>
-          <div className="underline"></div>
-        </div>
-        <div className="input-box">
-          <input type="password" placeholder="Enter Your Password" required/>
-          <div className="underline"></div>
-        </div>
-        <div className="input-box button">
-          <input type="submit" name="" value="Continue"/>
-        </div>
-      </form>
+    <div className="login3-body">
+      <div className="login3-container">
+        <form action="#" className="login3-form">
+          <div className="login3-title">Login</div>
+          <div className="login3-input-box login3-underline">
+            <input
+              className="login3-input"
+              type="text"
+              placeholder="Enter Your Email"
+              required
+            />
+            <div className="login3-underline"></div>
+          </div>
+          <div className="login3-input-box">
+            <input
+              className="login3-input"
+              type="password"
+              placeholder="Enter Your Password"
+              required
+            />
+            <div className="login3-underline"></div>
+          </div>
+          <div className="login3-input-box login3-button">
+            <input
+              className="login3-input"
+              type="submit"
+              name=""
+              value="Continue"
+            />
+          </div>
+        </form>
         <div className="option">or Connect With Social Media</div>
-        <div className="twitter">
-          <Link href="#"><i className="fab fa-twitter"></i>Sign in With Twitter</Link>
+        <div className="login3-twitter">
+          <Link href="#">
+            <i className="fab fa-twitter"></i>Sign in With Twitter
+          </Link>
         </div>
-        <div className="facebook">
-          <Link href="#"><i className="fab fa-facebook-f"></i>Sign in With Facebook</Link>
+        <div className="login3-facebook">
+          <Link href="#">
+            <i className="fab fa-facebook-f"></i>Sign in With Facebook
+          </Link>
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Login3
+export default Login3;
